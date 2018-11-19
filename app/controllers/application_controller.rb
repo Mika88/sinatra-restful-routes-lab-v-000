74295311f0d7a9cf5329c247a77dcf9ec71fcaf7
Mika88 <mikaspivak@gmail.com>
2018-11-19 22:39:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.find_by_id(params[:id])
     erb :edit
   end
- 
+
   patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
